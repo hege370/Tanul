7bd31003465622b0f7a09f856773c9584e1c5c92
hege370 <129584031+hege370@users.noreply.github.com>
2024-02-12 14:33:04 +0100

    ok

---
+++ megoldasok2.sql
@@ -0,0 +1,32 @@
+﻿-- A feladatok megoldására elkészített SQL parancsokat illessze be a feladat sorszáma után!
+
+
+-- 10. feladat:
+
+
+
+-- 11. feladat:
+
+
+
+-- 12. feladat:
+
+
+
+-- 13. feladat:
+
+
+
+-- 14. feladat:
+
+
+
+
+-- 15. feladat:
+
+
+
+
+-- 16. feladat:
+
+
